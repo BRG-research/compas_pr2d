89b39756f5b7b361e4a4a498728695e40fa5a467
Baraa El Moussa <belmoussa@ethz.ch>
2026-02-07 17:14:25 +0100

    added the new functions to the __init__

--- src/compas_pr2d/solver/__init__.py
+++ src/compas_pr2d/solver/__init__.py
@@ -1,3 +1,3 @@
-from .cvxpy_backend import solve_cvxpy, deform_polygon_linear
+from .cvxpy_backend import solve_cvxpy, deform_polygon_linear, solve_cvxpy_dual
 
-__all__ = ["solve_cvxpy", "deform_polygon_linear"]
+__all__ = ["solve_cvxpy", "deform_polygon_linear", "solve_cvxpy_dual"]
